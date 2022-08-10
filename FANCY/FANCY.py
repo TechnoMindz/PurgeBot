4be6ba267d8c793bfb0f714b8b9b623505b65ec5
Techno Mindz <97782515+TechnoMindz@users.noreply.github.com>
2022-08-10 14:17:54 +0530

    TM Purge Bot

---
+++ FANCY/FANCY.py
@@ -0,0 +1,26 @@
+BRGIN = """—🧻••÷[ Techno_Mindz_Bot ]÷••🧻—  
+Hello user i have been coded to help groups with deleting muliple messages without any hassle.
+
+press /help if you want to use me
+
+ 🦞DΣV MΣПƬIӨП:
+💻 @TechnoMindzYt | @TechnoMindzChat  
+—🧻••÷[ Techno_Mindz_Bot ]÷••🧻—"""
+DEL_TER = "https://telegra.ph/file/1d478d8b9f179abcf6aee.jpg"
+FUSE = """—🧻••÷[ ɦʏքɛ_քʊʀɢɛ_ɮօȶ ]÷••🧻—  
+Firstly add the bot in group and make it admin with `delete-messages` permission only!
+
+Bot is ready.
+use any of these commands:
+  - /purge
+  - /delete
+  - /remove
+  - /clean
+  `[as a reply to the messages you want to delete]`
+  
+ 🦞DΣV MΣПƬIӨП:
+💻 @TechnoMindzYt | @TechnoMindzChat  
+—🧻••÷[ Techno_Mindz_Bot ]÷••🧻—"""
+IMPORTED = {}
+HELPABLE = {}
+GDPR = []
